f651ae0bdc5d949220917f5abf75848d38c94af6
Gustavo Braganca <ga.braganca@gmail.com>
2020-07-04 22:22:43 -0300

    Implement 'Not Found' response

--- src/api/app/settings.py
+++ src/api/app/settings.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.signals import after_task_publish
 
 SYNSPEC_PATH = os.getenv("SYNSPEC_DIRPATH")
 
@@ -9,3 +10,12 @@ celery_app = Celery(
     broker=os.getenv("CELERY_BROKER_URL"),
 )
 celery_app.conf.task_routes = {"app.tasks.run_synspec": "synspec-queue"}
+
+
+@after_task_publish.connect
+def update_sent_state(sender=None, headers=None, **kwargs):
+    task = celery_app.tasks.get(sender)
+    backend = task.backend if task else celery_app.backend
+
+    if backend.get_state(headers["id"]) == "PENDING":  # Task does not exists in backend
+        backend.store_result(headers["id"], None, "SENT")
